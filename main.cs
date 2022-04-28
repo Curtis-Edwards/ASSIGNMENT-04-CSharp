fd087f9a2865348395f859d3915b653dcdb96468
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-04-28 22:53:38 +0000

    fixed total price

--- main.cs
+++ main.cs
@@ -5,28 +5,48 @@ class Program
     public static void Main(string[] args)
     {
         // This function accepts user input
-        double pizzaprice;
-        double toppingprice;
-        double totalprice
-        double tax
-
+        var pizzaprice = 0.00;
+        var toppingprice = 0.00;
+        var totalprice = 0.00;
+        var tax = 0.00;
+        double pizzasize;
+        double toppings;
 
         Console.WriteLine("Please enter size of you pizza");
         Console.WriteLine("");
 
-        Console.Write("pizzaprice: ");
-        pizzaprice = Convert.ToInt32(Console.ReadLine());
+        Console.Write("pizza size? (1: lagre / 2 extralarge): ");
+        pizzasize = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-
-        if () {
-            
+        Console.Write("Toppings? : ");
+        toppings = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
+      
+        if ( pizzasize == 1 ) {
+        pizzaprice = 6.00;
         }
-        area = (pi * radius) * radius;
+        if ( pizzasize == 2 ) {
+        pizzaprice = 10.00;
+        }
 
+        if ( toppings  == 1 ) {
+        toppingprice = 1.00;
+        }
+        if ( toppings  == 2 ) {
+        toppingprice = 1.75;
+        }
+        if ( toppings  == 3 ) {
+        toppingprice = 2.25;
+        }
+        if ( toppings  == 4 ) {
+        toppingprice = 3.35;
+        }
+        
 
+        tax = ( pizzaprice + toppingprice ) * 0.13;
+        totalprice = ( pizzaprice + toppingprice ) + tax;
 
-
-        Console.WriteLine("Area: " + area.ToString("0.00") + "cm²");
+        Console.WriteLine("Your pizza will cost  $" + totalprice.ToString("0.00") );
 
         Console.WriteLine("\nDone.");
     }
